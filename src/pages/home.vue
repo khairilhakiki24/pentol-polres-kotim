5ae2f1f2985242b39ccc0f5d4643697782eed5cd
khairilhakiki24 <kikikrenz@gmail.com>
2022-09-12 21:02:49 +0800

    Add files via upload

---
+++ src/pages/home.vue
@@ -0,0 +1,26 @@
+<template>
+	<f7-page name="home">
+		<!-- Top Navbar -->
+		<f7-navbar :sliding="true">
+			<!-- <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left> -->
+			<f7-nav-title sliding>COBEST POLRES KOTIM</f7-nav-title>
+			<!-- <f7-nav-right>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
+      </f7-nav-right> -->
+			<!-- <f7-nav-title-large>Cobest App</f7-nav-title-large> -->
+		</f7-navbar>
+
+	
+		<iframe
+			src="https://docs.google.com/forms/d/e/1FAIpQLSeqC1WxI63fbftxUN1MoVXHhrXo_lqbwbKmANUgT52qCzvIxg/viewform"
+			width="100%"
+			height="100%"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0"
+			>Memuat…</iframe
+		>
+	</f7-page>
+</template>
---
+++ src/pages/settings.vue
@@ -0,0 +1,63 @@
+<template>
+	<f7-page name="settings">
+		<f7-navbar title="COBEST POLRES KOTIM"></f7-navbar>
+		<!-- Page content-->
+		<!-- <f7-block strong>
+			<p>Jadwal Kunjungan Tahanan</p>
+		</f7-block> -->
+		<f7-block strong>
+			<p><b>Dibawah ini adalah jadwal kunjungan tahanan :</b></p>
+			<ul>
+				<li>Hari Senin jam 09.30 - 13.30</li>
+				<li>Hari Kamis jam 09.00 - 13.30</li>
+			</ul>
+			<p>
+				Keluarga tahanan dipersilahkan untuk melakukan video call dari rumah
+				setiap hari pada jam kerja 09.00 - 13.30.
+			</p>
+		</f7-block>
+		<f7-block strong>
+			<p><b>Kantor</b></p>
+			<p>
+				Jl. Jendral Sudirman Km. 0 <br />
+				Sampit, Kabupaten Kotawaringin Timur <br />
+				Kalimantan Tengah <br />
+				Telp : 0531-21110
+			</p>
+		</f7-block>
+
+		<f7-block strong>
+			<p><b>Social Media</b></p>
+			<ul>
+				<li>
+					<a href="https://www.instagram.com/polreskotim_sampit"
+						><img
+							width="50"
+							src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c521.png"
+					/></a>
+				</li>
+				<li>
+					<a href="https://www.youtube.com/channel/UC_ibboBavkOYXfcTNeyPbIA"
+						><img
+							width="50"
+							src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c545.png"
+					/></a>
+				</li>
+			</ul>
+		</f7-block>
+	</f7-page>
+</template>
+
+<style lang="scss">
+ul {
+	padding: 0;
+	display: flex;
+	align-content: center;
+	align-items: center;
+}
+
+ul > li {
+	list-style: none;
+	margin-right: 10px;
+}
+</style>
